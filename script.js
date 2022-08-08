bc9466636da480e879325f10a576990eebfb680a
zeuselijah <simeonhendricks@gmail.com>
2022-08-08 13:52:43 -0400

    wip: events lab updates

--- script.js
+++ script.js
@@ -8,6 +8,8 @@ const body = document.querySelector('body');
 
 // event listeners - code that handles DOM events
 btn.addEventListener('click', handleButtonPress);
+input.addEventListener('keydown', handleEnterPress);
+
 
 // body.addEventListener('click', handleButtonPress); 
 ul.addEventListener('click', handleLiClick);
@@ -20,6 +22,12 @@ in order to make it easier to maintain
 
 
 // functions
+function handleEnterPress(event) {
+    console.log(keypress)
+    if(event.keyCode === 13){
+        handleButtonPress(event);
+    }
+}
 
 function handleButtonPress(event) {
     // let's creat a DOM element so we can add comments to the list
@@ -38,13 +46,17 @@ function handleButtonPress(event) {
 function handleLiClick(event) {
     // only allow for li tags to trigger events
     if(event.target.tagName === 'LI'){
+        // write the code that will change the color of the text
+        event.target.style.color = 'rebeccapurple';
         if(event.target.style.textDecoration === 'none'){
         event.target.style.textDecoration = 'line-through';
-    } else {
+        } else {
         event.target.style.textDecoration = 'none'
-    }
+        }
     }
   }  
+
+
     // console.log('something inside the ul tag was clicked');
 
 
